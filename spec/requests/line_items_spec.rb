76d7f47946fbf8bd51f3350a22e2852358b14187
danielb78 <boldan.daniel@gmail.com>
2017-06-06 01:17:35 +0300

    Highlighting cart changes

--- spec/requests/line_items_spec.rb
+++ spec/requests/line_items_spec.rb
@@ -9,13 +9,21 @@ RSpec.describe 'LineItems', type: :request do
   end
 
   describe 'POST /line_items' do
-    it 'create line item' do
-      product = create(:product)
+    let(:product) { create(:product) }
 
+    it 'create line item' do
       post line_items_url, params: { product_id: product.id}
       follow_redirect!
       assert_select 'h2', 'Your Cart'
       assert_select 'td', 'Title'
+    end
+
+    it 'create line item via ajax' do
+      post line_items_url, params: { product_id: product.id}, xhr: true
+      assert_response :success
+      assert_select_jquery :html, '#cart' do
+        assert_select 'tr#current_item td', /Title/
+      end
     end
   end
 end
