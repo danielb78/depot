4d886a5de17edf9ae51c0ebcdae9e54ff8790095
danielb78 <boldan.daniel@gmail.com>
2017-06-05 03:54:08 +0300

    Iteration D3 - add to cart

--- spec/requests/line_items_spec.rb
+++ spec/requests/line_items_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "LineItems", type: :request do
-  describe "GET /line_items" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'LineItems', type: :request do
+  describe 'GET /line_items' do
+    it 'works! (now write some real specs)' do
       get line_items_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST /line_items' do
+    it 'create line item' do
+      product = create(:product)
+
+      post line_items_url, params: { product_id: product.id}
+      follow_redirect!
+      assert_select 'h2', 'Your Cart'
+      assert_select '#main li', 'Title'
     end
   end
 end
